3850064a7881020b569b02f44b7004457d51bce6
noeinatha <92200039+noeinatha@users.noreply.github.com>
2023-08-23 16:25:38 +0700

    Class03 code

---
+++ ReactWeb/car-rental-app/src/app/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Banner from "@/components/Banner";
+import ProductCard from "@/components/ProductCard";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <Banner />
+        <div
+          style={{
+            margin: "20px",
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+            alignContent: "space-around",
+          }}
+        >
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </div>
+    </main>
+  );
+}
