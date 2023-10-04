52e43934d39c849c9e53216c6db6fa3cd6240367
noeinatha <92200039+noeinatha@users.noreply.github.com>
2023-10-04 22:19:03 +0700

    Class08 code

--- ReactWeb/car-rental-app/src/app/page.tsx
+++ ReactWeb/car-rental-app/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Banner from "@/components/Banner";
-import CarPanel from "@/components/CarPanel";
+import TravelCard from "@/components/TravelCard";
 
 export default function Home() {
   return (
     <main>
       <div>
         <Banner />
+        <TravelCard></TravelCard>
       </div>
     </main>
   );
---
+++ ReactWeb/car-rental-app/src/components/VlogPlayer.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useRef, useEffect, useState } from "react";
+import { useWindowListener } from "@/hooks/useWindowListener";
+
+export default function VlogPlayer({
+  vdoSrc,
+  isPlaying,
+}: {
+  vdoSrc: string;
+  isPlaying: boolean;
+}) {
+  const vdoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    // alert("width is" + vdoRef.current?.videoWidth);
+    if (isPlaying) {
+      // alert("play vdo");
+      vdoRef.current?.play();
+    } else {
+      // alert("pause vdo");
+      vdoRef.current?.pause();
+    }
+  }, [isPlaying]);
+
+  useWindowListener("resize", (e) => {
+    alert("window width is " + (e.target as Window).innerWidth);
+  });
+
+  return (
+    <video className="w-[40%]" src={vdoSrc} ref={vdoRef} controls loop muted />
+  );
+}
