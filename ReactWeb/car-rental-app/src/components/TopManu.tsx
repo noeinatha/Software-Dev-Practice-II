0f4e3667fa94db45d5938db1387be9501d7fd39d
noeinatha <92200039+noeinatha@users.noreply.github.com>
2023-10-19 21:32:37 +0700

    Class10 code

--- ReactWeb/car-rental-app/src/components/TopManu.tsx
+++ ReactWeb/car-rental-app/src/components/TopManu.tsx
@@ -1,8 +1,13 @@
 import styles from "./topmanu.module.css";
 import Image from "next/image";
 import TopManuItem from "./TopManuItem";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { Link } from "@mui/material";
 
-export default function TopManu() {
+export default async function TopManu() {
+  const session = await getServerSession(authOptions);
+
   return (
     <div className={styles.manucontainer}>
       <Image
@@ -15,6 +20,20 @@ export default function TopManu() {
       ></Image>
       <TopManuItem title="Reservations" pageRef="/reservations"></TopManuItem>
       <TopManuItem title="About" pageRef="/about"></TopManuItem>
+
+      {session ? (
+        <Link href="/api/auth/signout">
+          <div className="flex item-center absolute right-0 h-full p-2 text-cyan-600 text-md">
+            Sign-Out of {session.user?.name}
+          </div>
+        </Link>
+      ) : (
+        <Link href="/api/auth/signin">
+          <div className="flex item-center absolute right-0 h-full p-2 text-cyan-600 text-md">
+            Sign-In
+          </div>
+        </Link>
+      )}
     </div>
   );
 }
